63a7e22edf314bc10e72a0ec67a8c3954c496131
Epistemopholic <front8nd@gmail.com>
2024-07-07 16:00:26 +0500

    init

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
-//Connect to MongoDB
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
+    console.error("Error connecting to MongoDB", error);
+    throw error;
   }
 };
 
